b1986551e2973f998f4f5297e4332e1ac0c4720e
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-25 21:03:15 -0700

    fix ajax request

--- public/js/application.js
+++ public/js/application.js
@@ -1,17 +1,12 @@
 $(document).ready(function(){
   $('#get_color').on('click', function(e) {
-        $(this).alert("inside the click");jjhbkj
-        // console.log("this is e" + e);
-        // console.log($(this));
-    // e.preventDefault();
-    // $.ajax({
-    //   url: '/color',
-    //   type: 'POST'
-    // }).done(function(result){
-    //   alert("we are here");
-      // console.log("it's done!" + result);
-      // $('#color_me li').text(result).show();
-      // window.location = "/";
-    // });
+    e.preventDefault();
+    $.ajax({
+      url: '/color',
+      type: 'POST'
+    }).done(function(result){
+      console.log(result);
+      $('#color_me li:nth-child(' + result[0] + ')').css({'background-color': result[1]});
+    });
   });
 });
